dd26b26cd726c67a3089d0abb7ff258246feee68
elsa <elsa.sar.ali@gmail.com>
2021-06-02 14:26:46 +0200

    use the new loomchild-segmente package to manage optional Loomchild dependency delete segment submodule remove LoomchildSegmenter class

--- setup.py
+++ setup.py
@@ -3,58 +3,31 @@
 import setuptools
 import subprocess
 import os.path
-import shutil
-from sys import argv
-
-def compile_loomchild(current_path):
-    jars = ["activation-1.1.1.jar",  "commons-cli-1.2.jar",  "commons-logging-1.1.1.jar",
-        "gson-2.8.0.jar",  "jaxb-api-2.3.0.jar",  "jaxb-core-2.3.0.jar",
-        "jaxb-impl-2.3.0.jar",  "junit-4.1.jar",  "segment-2.0.2-SNAPSHOT.jar",
-        "segment-2.0.2-SNAPSHOT-tests.jar",  "segment-ui-2.0.2-SNAPSHOT.jar"]
-
-    all_compiled = True
-    for f in jars:
-        if not os.path.isfile(os.path.join(current_path, "bifixer/data/segment-2.0.2-SNAPSHOT/lib", f)):
-            all_compiled = False
-            break
-
-    if not all_compiled:
-        subprocess.check_call(["mvn", "clean", "install"], cwd=os.path.join(current_path, "segment/segment"))
-        subprocess.check_call(["mvn", "clean", "install"], cwd=os.path.join(current_path, "segment/segment-ui"))
-        subprocess.check_call(["unzip", "-o", os.path.join(current_path, "segment/segment-ui/target/segment-2.0.2-SNAPSHOT.zip"), 
-            "segment-2.0.2-SNAPSHOT/lib/*", "-d", os.path.join(current_path, "bifixer/data")])
-
-    shutil.copytree(os.path.join(current_path, "segment/srx"), os.path.join(current_path, "bifixer/data/srx"), dirs_exist_ok=True)
 
 if __name__=="__main__":
     with open("README.md", "r") as fh:
         long_description = fh.read()
     with open("requirements.txt") as rf:
         requirements = rf.read().splitlines()
-
-    package_data_list=["replacements/*"] 
-
-    if "--install-loomchild" in argv:
-        argv.remove("--install-loomchild")
-        compile_loomchild(os.path.dirname(os.path.abspath(__file__)))
-        package_data_list.append("data/segment-2.0.2-SNAPSHOT/lib/*.jar")
-        package_data_list.append("data/srx/*.srx")
+    with open("optional-requirements.txt") as ef:
+        extras_requirements = ef.read().splitlines()
 
     setuptools.setup(
         name="bifixer",
-        #version="",
+        version="0.1",
         install_requires=requirements,
+        extras_require={"loomchild": extras_requirements},
         license="GNU General Public License v3.0",
         author="Prompsit Language Engineering",
-        #author_email="",
-        #maintainer="",
-        #maintainer_email="",
-        #description="",
+        author_email="info@prompsit.com",
+        description="Tool to fix bitexts and tag near-duplicates for removal",
+        maintainer="Marta Bañon",
+        maintainer_email="mbanon@prompsit.com",
         long_description=long_description,
         long_description_content_type="text/markdown",
         url="https://github.com/bitextor/bifixer",
         packages=["bifixer"],
-        package_data={"bifixer": package_data_list},
+        package_data={"bifixer": ["replacements/*"]},
         classifiers=[
             "Environment :: Console",
             "Intended Audience :: Science/Research",
@@ -71,5 +44,5 @@ if __name__=="__main__":
             "Prompsit Language Engineering": "http://www.prompsit.com",
             "Paracrawl": "https://paracrawl.eu/"
              },
-        scripts=["scripts/bifixer"]     
+        scripts=["scripts/bifixer"]
     )
