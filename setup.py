940995f83801438441b76cbffec087ddb23d7eb5
Marta Bañón <34235767+mbanon@users.noreply.github.com>
2021-12-03 17:08:18 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ if __name__=="__main__":
 
     setuptools.setup(
         name="bifixer",
-        version="0.6",
+        version="0.6.1",
         install_requires=requirements,
         extras_require={"loomchild": extras_requirements},
         license="GNU General Public License v3.0",
