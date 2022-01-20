c148bbdac5b10277006547dc121d5f8e6ed25196
Cristian García Romero <cgr71ii@gmail.com>
2022-01-20 10:42:44 +0100

    Output header will take the input header value
    If --header is set, --output_header will use the input header
    instead of the provided header to the flag

--- bifixer/bifixer.py
+++ bifixer/bifixer.py
@@ -59,7 +59,7 @@ def initialization():
     groupO = parser.add_argument_group('Optional')
     # Format
     groupO.add_argument("--header", action='store_true', help="Input file will have header")
-    groupO.add_argument("--output_header", help="Output file header (separated by ',')")
+    groupO.add_argument("--output_header", action='store_true' if header else None, help="Output file header (separated by ','). If --header is set, those values will be used if this flag is set")
     groupO.add_argument("--scol", type=util.check_positive if not header else str, default=3 if not header else "src_text", help="Source sentence column (starting in 1). The name of the field is expected instead of the position if --header is set")
     groupO.add_argument("--tcol", type=util.check_positive if not header else str, default=4 if not header else "trg_text", help="Target sentence column (starting in 1). The name of the field is expected instead of the position if --header is set")
     groupO.add_argument("--sdeferredcol", type=util.check_positive if not header else str, help="Source deferred standoff annotation column (starting in 1). The name of the field is expected instead of the position if --header is set")
@@ -160,8 +160,16 @@ def fix_sentences(args):
             args.tdeferredcol = int(header.index(args.tdeferredcol)) + 1
 
     if args.output_header:
+        if args.header:
+            if not 'header' in locals():
+                raise Exception("Unexpected: 'header' should be defined")
+
+            output_header = header
+        else:
+            output_header = args.output_header.strip().split(",")
+
         # Write the output header once
-        args.output.write("\t".join(args.output_header.strip().split(",")))
+        args.output.write("\t".join(output_header))
 
         if args.dedup:
             args.output.write("\tbifixer_hash\tbifixer_score")
